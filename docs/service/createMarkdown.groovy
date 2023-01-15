1a492c6a0193aa51f59278adacc7dfc449f7c401
Egon Willighagen <egon.willighagen@gmail.com>
2023-01-15 17:08:29 +0100

    Take a list of JSON files

--- docs/service/createMarkdown.groovy
+++ docs/service/createMarkdown.groovy
@@ -1,24 +1,33 @@
 // Copyright (C) 2023  Egon Willighagen
 // License: MIT
 
+// groovy createMarkdown.groovy *.json
+
 import groovy.json.JsonSlurper
 
-dataFile = args[0]
 templateFile = "template.md"
 
-fileContents = new File(dataFile).text
-def data = new JsonSlurper().parseText(fileContents)
+args.each { dataFile ->
+if (dataFile != "template.json") {
+  println dataFile
+  fileContents = new File(dataFile).text
+  def data = new JsonSlurper().parseText(fileContents)
 
-lines = new File(templateFile).readLines()
-lines.each { String line ->
-  if (line.contains("%id%")) { println line.replaceAll("%id%", data.id) } else
-  if (line.contains("%service%")) { println line.replaceAll("%service%", data.service) } else
-  if (line.contains("%url%")) { println line.replaceAll("%url%", data.url) } else
-  if (line.contains("%description%")) { println line.replaceAll("%description%", data.description) } else
-  if (line.contains("%screenshot%")) { println line.replaceAll("%screenshot%", data.screenshot) } else
-  if (line.contains("%provider.name%")) { println line.replaceAll("%provider.name%", data.provider.name) } else
-  if (line.contains("%provider.contact.name%")) { println line.replaceAll("%provider.contact.name%", data.provider.contact.name) } else
-  if (line.contains("%access.login%")) { println line.replaceAll("%access.login%", data.access.login) } else
-  { println line }
+  outputFile = new File(data.id + ".md")
+  outputFile.text = ""
+
+  lines = new File(templateFile).readLines()
+  lines.each { String line ->
+    if (line.contains("%id%")) { outputFile << line.replaceAll("%id%", data.id) } else
+    if (line.contains("%service%")) { outputFile << line.replaceAll("%service%", data.service) } else
+    if (line.contains("%url%")) { outputFile << line.replaceAll("%url%", data.url) } else
+    if (line.contains("%description%")) { outputFile << line.replaceAll("%description%", data.description) } else
+    if (line.contains("%screenshot%")) { outputFile << line.replaceAll("%screenshot%", data.screenshot) } else
+    if (line.contains("%provider.name%")) { outputFile << line.replaceAll("%provider.name%", data.provider.name) } else
+    if (line.contains("%provider.contact.name%")) { outputFile << line.replaceAll("%provider.contact.name%", data.provider.contact.name) } else
+    if (line.contains("%access.login%")) { outputFile << line.replaceAll("%access.login%", data.access.login) } else
+    { outputFile << line }
+    outputFile << "\n"
+  }
 }
-
+}
